a28fb5ee9d813e0fb22a6e393c1aee4953809f84
Ashutosh Mishra <ashutoshmishra097915@gmail.com>
2021-11-19 17:02:19 +0530

    feat: Added a image display holder

--- lib/screens/home/controller/home.dart
+++ lib/screens/home/controller/home.dart
@@ -1,7 +1,14 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
 
 class HomeController extends GetxController {
+  var result = ''.obs;
+  ImagePicker? imagePicker;
+  File? imageFile;
+
   final _picker = ImagePicker();
   final imagePath = ''.obs;
   final imageUrl = ''.obs;
@@ -11,6 +18,7 @@ class HomeController extends GetxController {
     final _pickedFile = await _picker.pickImage(source: imageSource);
     if (_pickedFile != null) {
       imagePath.value = _pickedFile.path;
+      doImageClassification();
     } else {
       Get.snackbar(
         'Error',
@@ -19,6 +27,42 @@ class HomeController extends GetxController {
         animationDuration: const Duration(seconds: 1),
       );
     }
-    notifyChildrens();
+    update();
+  }
+
+  loadDataModelFiles() async {
+    Future<String?> output = Tflite.loadModel(
+      model: 'assets/datasets/model_unquant.tflite',
+      labels: 'assets/datasets/labels.txt',
+      numThreads: 1,
+      isAsset: true,
+      useGpuDelegate: false,
+    );
+    print(output);
+  }
+
+  @override
+  void onInit() {
+    imagePicker = ImagePicker();
+    loadDataModelFiles();
+    super.onInit();
+  }
+
+  doImageClassification() async {
+    var recognition = await Tflite.runModelOnImage(
+      path: imagePath.value,
+      imageMean: 0.0,
+      imageStd: 255.0,
+      numResults: 1,
+      threshold: 0.1,
+      asynch: true,
+    );
+    print(recognition!.length.toString());
+    result.value = '';
+    update();
+    recognition.forEach((element) {
+      print(element.toString());
+      result.value += element['label'] + '\n\n';
+    });
   }
 }
--- lib/screens/home/ui/home.dart
+++ lib/screens/home/ui/home.dart
@@ -1,35 +1,79 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:recyclo/screens/home/controller/home.dart';
 import 'package:recyclo/screens/home/ui/widgets/image_option.dart';
-import 'package:recyclo/utils/shared/app_colors.dart';
 import 'package:recyclo/utils/shared/ui_helpers.dart';
 import 'package:recyclo/utils/widgets/box_button.dart';
+import 'package:recyclo/utils/widgets/box_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final HomeController homeController = Get.put(HomeController());
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-            onTap: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (ctx) => const ImageOptions(),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-              );
-            },
+      appBar: AppBar(
+        title: const BoxText.headingThree(
+          'Recyclo',
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(40.0),
             child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: const BoxButton(
-                title: 'Camera',
-              ),
-            )),
+              height: screenHeightPercentage(context, percentage: 0.3),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: (homeController.imagePath.value != '')
+                  ? Obx(() {
+                      return Image.file(
+                        File(homeController.imagePath.value),
+                        height: 160.0,
+                        width: 400.0,
+                        fit: BoxFit.contain,
+                      );
+                    })
+                  : const SizedBox(
+                      width: 140.0,
+                      height: 190.0,
+                      child: Icon(
+                        Icons.image,
+                        color: Colors.black38,
+                        size: 80.0,
+                      ),
+                    ),
+            ),
+          ),
+          GestureDetector(
+              onTap: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (ctx) => const ImageOptions(),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                  vertical: 20.0,
+                ),
+                child: const BoxButton(
+                  title: 'Camera',
+                ),
+              )),
+        ],
       ),
     );
   }
